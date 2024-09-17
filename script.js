1fc8acaea085f73fefbfc82c02de7fb7a9d457ac
sansori-orim <kaungpyae240706aungmyin@gmail.com>
2024-09-17 09:17:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const wapper = document.querySelector('.wapper');
 const question = document.querySelector('.question');
 const yesBtn = document.querySelector('.yes-btn');
 const noBtn = document.querySelector('.no-btn');
+const img = document.querySelector('img');
 const wapperRect = wapper.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
 yesBtn.addEventListener('click',() => {
-    question.innerHTML = 'ဟုတ်ကဲ့..နောက်တစ်ခါမဆိုးတော့ပါဘူးနော်..';
+    question.innerHTML = 'ရေးးး ပိုက်ပိုက်တေ စုထားတော့မယ်..'
+    img.src = "st,small,507x507-pad,600x600,f8f8f8.jpg";
 });
 noBtn.addEventListener('mouseover',() => {
     const i = Math.floor(Math.random() * (wapperRect.width - noBtnRect.width)) +1;
